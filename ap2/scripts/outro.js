0f887b9e652eb66185ad02645160a22a61ec9d01
João Gois de Otoni <127162053+JGois1@users.noreply.github.com>
2023-11-27 21:20:31 -0300

    Delete ap2 directory

--- ap2/scripts/outro.js
+++
@@ -1,4 +0,0 @@
-const params = new URLSearchParams(window.location.search)
-
-document.getElementById('id').innerHTML = params.get('id');
-document.getElementById('nome').innerHTML = params.get('nome_completo');
